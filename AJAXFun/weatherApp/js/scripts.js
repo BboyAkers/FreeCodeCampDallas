9afa3c4ed2193d1897cfccd5a2ed8865cfd1244c
Thomas Ibarra <tibarra@nobilishealth.com>
2017-04-23 14:14:46 -0500

    weather app from thomas group

--- AJAXFun/weatherApp/js/scripts.js
+++ AJAXFun/weatherApp/js/scripts.js
@@ -1,20 +1,30 @@
 console.clear();
+
+function kelvinToAmerican (degreesKelvin) {
+    return (9/5) * degreesKelvin - 459.67;
+}
+
 //creating the request to send the server
 var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-	//4 means we recieved the response
-	if(xhr.readyState === 4) {
-    let data = JSON.parse(xhr.responseText);
-    document.querySelector('#temp').innerHTML = data.main.temp;
-    
-    
-    document.querySelector('#temp').innerHTML = data.main.temp;
-    
-    
-		document.getElementById('ajax').innerHTML = JSON.stringify(data, null, 4);
-	}
+
+xhr.onreadystatechange = function () {
+    //4 means we recieved the response
+    if (xhr.readyState === 4) {
+        let data = JSON.parse(xhr.responseText);
+
+        let temperature = kelvinToAmerican(data.main.temp);
+        document.querySelector('#temp').innerHTML = temperature.toFixed(0);
+
+        document.querySelector('#weather-main').innerHTML = data.weather[0].main;
+        document.querySelector('#weather-details').innerHTML = data.weather[0].description;
+
+        let icon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+        document.querySelector('#weather-icon').src = icon;
+
+        document.getElementById('ajax').innerHTML = JSON.stringify(data, null, 4);
+    }
 };
-xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?zip=75093&appid={{API_KEY}}');
+xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?zip=75093&appid=bcc318753fc77300a6a78da4d915f6bd');
 xhr.send();
 
 console.log(xhr);
